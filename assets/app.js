3330472f72af05c3c7bbaa55ee393d9ba864a223
harunkaraca <karacalar500@gmail.com>
2025-07-30 16:57:13 +0300

    July/30 second commit

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,20 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 
+import './styles/app.css';
+import './styles/components/header.css';
+import './styles/components/utility.css';
+import './styles/components/hero_section.css';
+import './styles/components/card_featured.css';
+import './styles/components/accordion_item.css';
+import './styles/components/bibliography.css';
+import './styles/components/scrollbar.css';
+import './styles/components/social_buttons.css';
+import './styles/components/social_links.css';
+import './styles/components/fonts.css';
+import './styles/components/root.css';
+import './styles/components/list_group.css';
+import './styles/components/footer.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
